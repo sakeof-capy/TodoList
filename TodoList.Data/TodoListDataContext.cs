e79cc606113805af78d48367addae9263a7af69f
Fourier2718281828 <non-linear@ukr.net>
2024-03-31 13:21:42 +0300

    added: users data context

---
+++ TodoList.Data/TodoListDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Data;
+
+public class TodoListDataContext : DbContext
+{
+    public TodoListDataContext(DbContextOptions<TodoListDataContext> options)
+        : base(options)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
+    }
+
+    public DbSet<TodoListUser> Users { get; set; }
+}
