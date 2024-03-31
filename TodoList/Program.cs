8960c2166265f7e3deffc590aacf4119cd11134d
Fourier2718281828 <non-linear@ukr.net>
2024-03-31 13:23:14 +0300

    added: TodoListDataContext registration to the di

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TodoListDataContext>(
+    optionsBuilder => optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("TodoListDb"))
+);
 
 var app = builder.Build();
 
